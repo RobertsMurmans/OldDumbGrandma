f645681bd5598e0ed6d5cbe4226e6d56cf011f18
RobertsMurmans <112924947+RobertsMurmans@users.noreply.github.com>
2023-09-22 19:17:19 +0300

    Added ability to change between menus

--- main.py
+++ main.py
@@ -13,19 +13,31 @@ layoutInput = [ [sg.Text("Log your fruit.")],
                 [sg.Text("Spiecies: "), sg.InputText()],
                 [sg.Text("Amount: "), sg.Slider(range = (1,256), orientation = 'h'), sg.Text("kg")],
                 [sg.Text("Is it a fruit or vegetable?: "), sg.Radio("Fruit", group_id="fruit"), sg.Radio("Vegetable", group_id="fruit")],
-                [sg.Button("Add"), sg.Button("Done")] ]
+                [sg.Button("Add"), sg.Button("Back")] ]
 
 #layout for Output section
-layoutOutput = []
+layoutOutput = [[sg.Button("Back")]]
 
-window = sg.Window('Gain reporter', layoutMain)
+layout = [[sg.Column(layoutMain, key="Main"), sg.Column(layoutInput, visible=False, key="Input"), sg.Column(layoutOutput, visible=False, key="Output")]]
+
+window = sg.Window('Gain reporter', layout)
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Done': #If the window is closed or the user is done it closes the window
             break
 
     if event == 'Add stock':
-        window.close()
-        window = sg.Window('Gain reporter', layoutInput)
+        window[f'Input'].update(visible=True)
+        window[f'Main'].update(visible=False)
+
+    if event == 'View inventory':
+        window[f'Output'].update(visible=True)
+        window[f'Main'].update(visible=False)
+         
+    if event == 'Back':
+        window[f'Input'].update(visible=False)
+        window[f'Output'].update(visible=False)
+        window[f'Main'].update(visible=True)
+          
 
 window.close()
